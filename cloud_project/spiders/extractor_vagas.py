0968279098503c8a8f69ae99d958cff8272255f6
GuiPimenta--Dev <guialvespimenta27@gmail.com>
2021-04-13 22:32:49 -0300

    version 1

--- cloud_project/spiders/extractor_vagas.py
+++ cloud_project/spiders/extractor_vagas.py
@@ -7,8 +7,8 @@ class ExtractorVagasSpider(scrapy.Spider):
     name = 'spider_jobs_vagas'
     start_urls = []
     item = JobsVagasItem()
-    # job = ["Python", "Java", "C#", "JavaScript", "Oracle", "RPA", "Flutter", "Designer"]
-    job = ["Python"]
+    job = ["Python", "Java", "C#", "JavaScript", "Oracle", "RPA", "Flutter", "Designer"]
+    # job = ["Python"]
 
     def __init__(self, *args, **kwargs):
 
