7a19d39303a3e61e6f804b6ca8b96c9c40aa902c
GuiPimenta--Dev <guialvespimenta27@gmail.com>
2021-04-29 10:05:13 -0300

    variaveis ambiente heroku

--- cloud_project/spiders/extractor_indeed.py
+++ cloud_project/spiders/extractor_indeed.py
@@ -8,9 +8,9 @@ class ExtractorIndeedSpider(scrapy.Spider):
     name = 'spider_jobs_indeed'
     start_urls = []
     item = JobsVagasItem()
-    job = ["Python", "Java", "C#", "JavaScript", "Oracle", "RPA", "Flutter", "Designer"]
+    # job = ["Python", "Java", "C#", "JavaScript", "Oracle", "RPA", "Flutter", "Designer"]
 
-    # job = ["Python"]
+    job = ["Python"]
 
     def __init__(self, *args, **kwargs):
 
