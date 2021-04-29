e1a26f80f9c6ff9d8809c93d0d9413181526b42d
GuiPimenta--Dev <guialvespimenta27@gmail.com>
2021-04-29 09:18:52 -0300

    variaveis de ambiente heroku

--- cloud_project/pipelines.py
+++ cloud_project/pipelines.py
@@ -20,7 +20,7 @@ class CloudProjectPipeline:
 
     def __init__(self):
         self.conn = pymongo.MongoClient(
-            f"mongodb+srv://{os.environ['USER']}:{os.environ['PASS']}@backend.lwkqa.mongodb.net/{os.environ['DB']}?retryWrites={os.environ['Retry']}&w=majority")
+            f"mongodb+srv://{os.environ['USER']}:{os.environ['PASS']}@backend.lwkqa.mongodb.net/{os.environ['DB']}?retryWrites={os.environ['RETRY']}&w=majority")
         # self.conn = pymongo.MongoClient(
         #     f"mongodb+srv://{USER}:{PASS}@backend.lwkqa.mongodb.net/{DB}?retryWrites={RETRY}&w=majority")
 
